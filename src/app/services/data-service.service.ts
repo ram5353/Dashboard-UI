f612c9ebe3b607e32a13eafa53372d48cdc8370e
ram5353 <ramkiran5353@gmail.com>
2021-11-01 16:34:51 -0400

    Initial commit

---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  ROOT_URL = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  public getTotalSales(): Observable<any> {
+    return this.http.get(this.ROOT_URL + '/transactions/totalsales');
+  }
+}
