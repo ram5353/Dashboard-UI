f17f7847ced36766b9f7b9c5ceb1c4634a340445
ram5353 <ramkiran5353@gmail.com>
2021-11-05 19:17:57 -0400

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HouseholdsComponent } from './components/households/households.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HouseholdsComponent
+    HouseholdsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  navbarOpen = false;
+
+  constructor(private route: Router) { }
+
+  ngOnInit() {
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  showCustomers() {
+    return this.route.navigate(['/fiber-customers']);
+  }
+
+}
